00a507258a025e9d457fec91934eff0697509721
SokkaScience <SokkaScience@github.com>
2024-06-21 17:59:42 +0100

    Initialised project w/ cargo

---
+++ target/debug/deps/galactic_guide-5f5476e30670e4f4.d
@@ -0,0 +1,5 @@
+/Users/charliewarner/Documents/Programming/Projects/Galactic-Guide/target/debug/deps/libgalactic_guide-5f5476e30670e4f4.rmeta: src/main.rs
+
+/Users/charliewarner/Documents/Programming/Projects/Galactic-Guide/target/debug/deps/galactic_guide-5f5476e30670e4f4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/galactic_guide.d
@@ -0,0 +1,1 @@
+/Users/charliewarner/Documents/Programming/Projects/Galactic-Guide/target/debug/galactic_guide: /Users/charliewarner/Documents/Programming/Projects/Galactic-Guide/src/main.rs
