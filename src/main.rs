f71ab36683abb050e79fe53fedebdf5eb3e0fe02
SokkaScience <SokkaScience@github.com>
2024-06-30 21:39:39 +0100

    Removed debug texture

--- src/main.rs
+++ src/main.rs
@@ -39,12 +39,6 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    // Setup debug material
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
-        ..default()
-    });
-
     // Create an earth using the CelestialBody struct
     let earth = CelestialBody::new("Earth", 5.0, Vec3::new(0.0, 0.0, 3.0));
     earth.spawn(&mut commands, &asset_server, &mut meshes, &mut materials);
@@ -80,34 +74,6 @@ fn setup(
         ));
 }
 
-// Creates a colorful test pattern
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::RENDER_WORLD,
-    )
-}
 
 // Orbit editor modal UI
 fn orbit_editor_ui(mut contexts: EguiContexts) {
